f93f39476d94807ca497a4fc62d6ab9639a9c98b
Linsanity-7 <l409506800@163.com>
2018-12-29 00:31:05 +0800

    commit file CommonController.java

---
+++ wechat-subscription-platform-controller/src/main/java/com/huazai/livehouse/wechat/subscription/platform/controller/CommonController.java
@@ -0,0 +1,11 @@
+package com.huazai.livehouse.wechat.subscription.platform.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lin
+ * @date 2018/12/29
+ */
+@RestController
+public class CommonController {
+}
