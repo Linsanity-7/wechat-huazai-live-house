ba3d305f5b975d2991ea4db828117c88117992e2
Linsanity-7 <l409506800@163.com>
2018-12-29 15:43:38 +0800

    commit update files

---
+++ wechat-subscription-platform-interface/src/main/java/com/huazai/livehouse/wechat/subscription/platform/service/CoreService.java
@@ -0,0 +1,18 @@
+package com.huazai.livehouse.wechat.subscription.platform.service;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @apiNote :核心服务类
+ * @author        :林尚华
+ * @date    : 2018/12/29 15:39
+ */
+public interface CoreService {
+    /**
+     * 处理微信发来的请求（包括事件的推送）
+     * @param request
+     * @return
+     */
+    String processRequest(HttpServletRequest request);
+}
---
+++ wechat-subscription-platform-service/src/main/java/com/huazai/livehouse/wechat/subscription/platform/service/impl/CoreServiceImpl.java
@@ -0,0 +1,19 @@
+package com.huazai.livehouse.wechat.subscription.platform.service.impl;
+
+import com.huazai.livehouse.wechat.subscription.platform.service.CoreService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 上海美市科技有限公司开发部
+ *
+ * @author        :林尚华
+ * @date    : 2018/12/29 15:43
+ * @Description:
+ */
+public class CoreServiceImpl implements CoreService {
+    @Override
+    public String processRequest(HttpServletRequest request) {
+        return null;
+    }
+}
